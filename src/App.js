3538ca73097bc179ebf9de9ae225ea4ef087571e
Antonio Conrado <antoniojconradojunior@hotmail.com>
2024-03-04 16:32:29 +0000

    Hero CSS Style

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import { Route, Router, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
+import Home from './components/pages/Home';
 
 function App() {
   return (
     <>
       <Navbar />
         <Switch>
-           <Route path='/' exact />
+           <Route path='/' exact component={Home}/>
         </Switch>
     </>
   );
