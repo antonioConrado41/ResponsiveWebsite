c7af8679483e8445e4ed1810264d7e62ba5112ba
Antonio Conrado <antoniojconradojunior@hotmail.com>
2024-03-11 20:38:13 +0000

    Router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import { Route, Router, BrowserRouter, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
+import Products from './components/pages/Products';
+import Services from './components/pages/Services';
+import SignUp from './components/pages/SignUp';
 
 function App() {
   return (
@@ -9,6 +12,9 @@ function App() {
       <Navbar />
         <Switch>
            <Route path='/' exact component={Home}/>
+           <Route path='/products' component={Products}/>
+           <Route path='/services' component={Services}/>
+           <Route path='/sign-up' component={SignUp}/>
         </Switch>
     </>
   );
