6b8a45405ec999334926a7f9d9d055436e652d5b
Antonio Conrado <antoniojconradojunior@hotmail.com>
2024-03-12 16:44:57 +0000

    last Part Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -24,14 +24,40 @@ function Footer() {
                 </form>
             </div>
         </section>
-        <div className='footer-links'>
-            <div className='footer-link-wraper'>
-                <div className='footer-link-items'>
-                    <h2> About us </h2>
-                    <Link to='sign-up'> How it works </Link>
-                    <Link to='/'> Testimonials </Link>
-                </div>
-            </div>
+        <div class='footer-links'>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>About Us</h2>
+            <Link to='/sign-up'>How it works</Link>
+            <Link to='/'>Testimonials</Link>
+            <Link to='/'>Careers</Link>
+            <Link to='/'>Investors</Link>
+            <Link to='/'>Terms of Service</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Contact Us</h2>
+            <Link to='/'>Contact</Link>
+            <Link to='/'>Support</Link>
+            <Link to='/'>Destinations</Link>
+            <Link to='/'>Sponsorships</Link>
+          </div>
+        </div>
+        <div className='footer-link-wrapper'>
+          <div class='footer-link-items'>
+            <h2>Videos</h2>
+            <Link to='/'>Submit Video</Link>
+            <Link to='/'>Ambassadors</Link>
+            <Link to='/'>Agency</Link>
+            <Link to='/'>Influencer</Link>
+          </div>
+          <div class='footer-link-items'>
+            <h2>Social Media</h2>
+            <Link to='/'>Instagram</Link>
+            <Link to='/'>Facebook</Link>
+            <Link to='/'>Youtube</Link>
+            <Link to='/'>Twitter</Link>
+          </div>
+        </div>
         </div>
         <section className='social-media'>
             <div className='social-media-wrap'>
@@ -64,8 +90,7 @@ function Footer() {
                         target='_blank'
                         aria-label='LinkedIn'>
                         <i className='fab fa-linkedin' />
-                        </Link>
-                        
+                        </Link>                
 
                     </div>
                 </div>
