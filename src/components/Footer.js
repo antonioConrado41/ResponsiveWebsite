4e385718a5d7fc7615a431fd5a507359906049a8
Antonio Conrado <antoniojconradojunior@hotmail.com>
2024-03-12 15:36:38 +0000

    update Footer + Logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import { Button } from 'react-bootstrap'
 import './Footer.css'
+import { Link } from 'react-router-dom/cjs/react-router-dom.min'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCode } from '@fortawesome/free-solid-svg-icons'
+
+const element = <FontAwesomeIcon icon={faCode} />
 
 function Footer() {
   return (
@@ -17,10 +22,56 @@ function Footer() {
                     <input type='email' name='email' placeholder='Your email' className='footer-input'/> 
                     <Button buttonStyle='btn--outline'> SUBSCRIBE </Button>
                 </form>
-
             </div>
-
         </section>
+        <div className='footer-links'>
+            <div className='footer-link-wraper'>
+                <div className='footer-link-items'>
+                    <h2> About us </h2>
+                    <Link to='sign-up'> How it works </Link>
+                    <Link to='/'> Testimonials </Link>
+                </div>
+            </div>
+        </div>
+        <section className='social-media'>
+            <div className='social-media-wrap'>
+                <div className='footer-logo'>
+                    <Link to='/' className='social-logo'>
+                        Dev {element}
+                    </Link>
+                    <small className='website-right'> DEV ® 2024 </small>
+                    <div className='social-icons'>
+                        <Link className='social-icon-link facebook' 
+                        to='/' 
+                        target='_blank'
+                        aria-label='Facebook'>
+                        <i className='fab fa-facebook-f' />
+                        </Link>
+                        <Link className='social-icon-link instagram' 
+                        to='/' 
+                        target='_blank'
+                        aria-label='Intagram'>
+                        <i className='fab fa-instagram' />
+                        </Link>
+                        <Link className='social-icon-link twitter' 
+                        to='/' 
+                        target='_blank'
+                        aria-label='Twitter'>
+                        <i className='fab fa-twitter' />
+                        </Link>
+                        <Link className='social-icon-link linkedin' 
+                        to='/' 
+                        target='_blank'
+                        aria-label='LinkedIn'>
+                        <i className='fab fa-linkedin' />
+                        </Link>
+                        
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
       
     </div>
   )
